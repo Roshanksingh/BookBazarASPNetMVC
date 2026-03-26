fab095ff9775e4247ea9d2bc31d9d920a1a17eba
Roshan Singh <singhroshan428@gmail.com>
2026-03-26 00:28:34 -0500

    Add project files.

---
+++ BookBazar.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using BookBazar.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookBazar.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BookBazar.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookBazar.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
