ebf1949a372b363f3cbdc59d48ef1be7eada30df
Roshan Singh <singhroshan428@gmail.com>
2026-03-28 00:31:51 -0500

    set applicationDbContext

---
+++ BookBazar.Web/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BookBazar.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
